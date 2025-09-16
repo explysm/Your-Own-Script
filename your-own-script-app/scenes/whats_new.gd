5ab3729baacdcabb592ce1df8aad1f204bdccb05
explysm <joshualtrost@gmail.com>
2025-09-16 00:25:58 -0500

    added more to the whats new screen

--- your-own-script-app/scenes/whats_new.gd
+++ your-own-script-app/scenes/whats_new.gd
@@ -2,19 +2,19 @@ extends CanvasLayer
 var config = ConfigFile.new()
 var closed
 func get_closed_value():
-     var err = config.load("user://user.cfg")
-     if err == OK:
-         closed = config.get_value("settings", "closed_whatsnew", "")
+	 var err = config.load("user://user.cfg")
+	 if err == OK:
+		 closed = config.get_value("settings", "closed_whatsnew", "")
 func _ready() -> void:
-    get_closed_value()
-    if closed == "":
-        show()
-    else:
-        hide()
-    
+	get_closed_value()
+	if closed == "":
+		show()
+	else:
+		hide()
+	
 
 
 func _on_button_pressed() -> void:
-    config.set_value("settings", "closed_whatsnew", "1")
-    config.save("user://user.cfg")
-    hide()
+	config.set_value("settings", "closed_whatsnew", "1")
+	config.save("user://user.cfg")
+	hide()
